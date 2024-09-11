f7b237be6a40921d32a11b03ff1e4c5045d841a0
Ivanha Paz <159570732+ivanhapaz@users.noreply.github.com>
2024-09-11 14:54:57 -0600

    Update JsonToYaml.tsx

--- components/seo/JsonToYaml.tsx
+++ components/seo/JsonToYaml.tsx
@@ -17,6 +17,34 @@ export default function JsonToYamlSEO() {
         </p>
       </section>
 
+        
+      <section>
+        <h2>More JSON conversion tools</h2>
+        <p>
+          Convert to CSV, query parameters, or beautify JSON with Jam's free
+          developer utilities. They're all available in dark mode too.
+        </p>
+        <ul>
+          <li>
+            <Link href="/utilities/csv-to-json">CSV to JSON</Link>: Easily
+            convert CSV data to JSON format—the quickest way to turn tabular
+            data into JSON for APIs and data processing.
+          </li>
+          <li>
+            <Link href="/utilities/yaml-to-json">YAML to JSON</Link>: Easily
+            convert human-readable YAML to JSON. Useful where you're working
+            with configuration files and need to switch between them.
+          </li>
+          <li>
+            <Link href="/utilities/query-params-to-json">
+              Query Parameters to JSON
+            </Link>
+            : Simplify data handling and integration in your web applications by
+            converting query strings to JSON.
+          </li>
+        </ul>
+      </section>
+
       <section>
         <h2>How to Use the JSON to YAML Converter</h2>
         <p>
